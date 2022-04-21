a547f30d453d24998ad85eee9a68bc92a58b1bfb
mgpai <64056966+mgpai22@users.noreply.github.com>
2022-04-21 18:23:35 -0400

    Update setup.py
    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='ergpy',
-    version='0.1.0',
+    version='0.1.1',
     description='Python-jvm wrapper for interacting with the Ergo Blockchain',
     long_description=long_description,
     long_description_content_type='text/markdown',
