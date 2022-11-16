f9bd479493f8447b86d18c6b7d27193ae68496ed
mgpai <64056966+mgpai22@users.noreply.github.com>
2022-11-15 23:23:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ stubgenj>=0.2.5
 # Metadata configuration
 setup(
     name='ergpy',
-    version='0.1.8.7',
+    version='0.1.9.0',
     description='Python-jvm wrapper for interacting with the Ergo Blockchain',
     long_description=long_description,
     long_description_content_type='text/markdown',
